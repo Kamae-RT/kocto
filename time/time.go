283eb125645ec90664292adda880c7dfda0e3c91
Maurizio Crocci <mc@kamae.pt>
2022-11-15 18:22:11 +0000

    Adds simple time utils

---
+++ time/time.go
@@ -0,0 +1,22 @@
+package time
+
+import (
+	"math"
+	"time"
+)
+
+const TimeLayout = "2006-01-02T15:04:05-0700"
+
+func TimeParse(s string) (time.Time, error) {
+	return time.Parse(TimeLayout, s)
+}
+
+func TicksFromTime(t time.Time) uint64 {
+	ticksPerMilli := 10_000
+	base := time.Date(1, 1, 1, 0, 0, 0, 0, time.UTC).UnixMilli()
+
+	baseTicks := ticksPerMilli * int(math.Abs(float64(base)))
+	ticks := t.UnixMilli() * int64(ticksPerMilli)
+
+	return uint64(baseTicks) + uint64(ticks)
+}
